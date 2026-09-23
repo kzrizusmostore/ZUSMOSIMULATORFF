1e75b8bf1309c735a35986b2600eb0c3b227a271
kzrizusmostore <kzrizusmostore@gmail.com>
2026-09-23 09:36:58 +0700

    Add files via upload

--- src/config/registry.js
+++ src/config/registry.js
@@ -12,12 +12,24 @@ export const MAPS = [
     environment: { fogNear: 180, fogFar: 520 }
   },
   {
+    id: 'dapur-mbg-kopdes',
+    name: 'Dapur MBG-KOPDES',
+    shortName: 'MBG',
+    asset: './assets/maps/dapur_mbg_kopdes.glb',
+    bytes: 6623792,
+    available: true,
+    bundled: true,
+    spawn: { x: 1.95, y: 0.15, z: -10.38, yaw: 0, dropHeight: 18 },
+    spawnRange: { xMin: -12, xMax: 16, yMin: -2, yMax: 20, zMin: -35, zMax: 14 },
+    environment: { fogNear: 80, fogFar: 220 }
+  },
+  {
     id: 'old-rampage',
     name: 'Old Rampage',
     shortName: 'OR',
     asset: './assets/maps/free_fire_old_rampage_lobby_3d_model.glb',
     bytes: 0,
-    available: true,
+    available: false,
     bundled: false,
     spawn: { x: 0, y: 0, z: 0, yaw: 0, dropHeight: 45 },
     spawnRange: { xMin: -180, xMax: 180, yMin: -10, yMax: 120, zMin: -180, zMax: 180 },
@@ -34,7 +46,8 @@ export const CHARACTERS = [
     bytes: 860760,
     available: true,
     bundled: true,
-    targetHeight: 1.72
+    targetHeight: 1.72,
+    rigged: true
   },
   {
     id: 'rouk',
@@ -44,7 +57,30 @@ export const CHARACTERS = [
     bytes: 1124240,
     available: true,
     bundled: true,
-    targetHeight: 1.72
+    targetHeight: 1.72,
+    rigged: false
+  },
+  {
+    id: 'rias-sexy',
+    name: 'Rias Sexy',
+    code: 'RS',
+    asset: './assets/characters/rias_sexy.glb',
+    bytes: 23098636,
+    available: true,
+    bundled: true,
+    targetHeight: 1.72,
+    rigged: false
+  },
+  {
+    id: 'amazing-spiderman',
+    name: 'The Amazing Spiderman',
+    code: 'SP',
+    asset: './assets/characters/the_amazing_spiderman.glb',
+    bytes: 810764,
+    available: true,
+    bundled: true,
+    targetHeight: 1.72,
+    rigged: false
   }
 ];
 
